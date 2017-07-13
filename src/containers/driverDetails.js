d9f70a0b8da5881581d8f0f9134c8951cc30931f
aswinmohanme <aswinmmohan@gmail.com>
2017-07-13 11:10:03 +0530

    Finish Driver Details Screen Skeleton

--- src/containers/driverDetails.js
+++ src/containers/driverDetails.js
@@ -1,9 +1,12 @@
 
 import React, { Component } from 'react';
 import Expo, {AppLoading} from 'expo';
-import {Text} from 'react-native-elements';
+import {Text, Avatar, Rating} from 'react-native-elements';
+import {View, StyleSheet} from 'react-native';
 
 import FireBase from '../stores/fireBase';
+import RideCard from '../components/rideCard';
+import FullButton from '../components/fullButton';
 
 class DriverDetailsScreen extends Component {
   constructor(props){
@@ -31,11 +34,54 @@ class DriverDetailsScreen extends Component {
   }
 
   render() {
-    if(this.state.didLoad)
-      return(<Text>{this.state.driver.name}</Text>)
-    else
-      return(<AppLoading />)
+    // if(this.state.didLoad)
+      return(
+        <View style={{flex: 1}}>
+        <View style={styles.container}> 
+          <View style={{margin: 40, marginTop:60 ,alignItems: 'center'}}>
+            <Avatar xlarge rounded source={{uri: 'https://api.adorable.io/avatars/285/amal@adorable.io.png'}}/>
+            <Text style={styles.name}>Aswin Mohan</Text>
+            <Text style={styles.phone}>8589931950</Text>
+            <Rating readonly imageSize={24} style={[styles.rating, {marginTop: 20}]}/>
+          </View>
+
+          <RideCard />
+          <View>
+            <Text> Ride Cost</Text>
+            <Text>$ 48</Text>
+          </View>
+          <View style={{flex: 1}}></View>
+          <FullButton onPressCall={() => navigate('BookTaxiScreen')} title="Come Pick Me Up" icon="paper-plane" />
+        </View>
+        </View>
+      );
+    // else
+      // return(<AppLoading />)
   }
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    margin: 24,
+    marginTop: Expo.Constants.statusBarHeight + 24,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    elevation: 50,
+    backgroundColor: 'white',
+  },
+  name: {
+    margin: 10,
+    marginTop: 24,
+    fontFamily: 'karla-bold',
+    fontSize: 18,
+  },
+  phone: {
+    fontSize: 18,
+    fontFamily: 'karla',
+  },
+  rating: {
+    backgroundColor: 'red',
+  }
+});
 export default DriverDetailsScreen;
